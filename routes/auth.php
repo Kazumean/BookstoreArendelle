c51eb68bbb337e71ab3cd7ece9377411e7afc1d9
Kazumean <kazumin.angelav@gmail.com>
2022-10-07 10:43:43 +0900

    ユーザー登録後の自動ログイン機能を削除し、ログイン画面に遷移するように仕様を変更。

--- routes/auth.php
+++ routes/auth.php
@@ -14,10 +14,10 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register_user');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])->name('user_registered');
+    Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+                ->name('login_user');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
